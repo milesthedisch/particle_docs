f84c6df6229de8784cc3af4c18f82d858467eb5d
Miles Disch <milesthedisch@gmail.com>
2016-11-29 17:59:31 +1100

    Ignore all node_modules

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const serve = require("./controllers/helpers/serve");
 const app = require("./controllers/app");
+<<<<<<< Updated upstream
 const particleLib = require("particle_library");
 
+=======
+const fs = require('fs');
+
+particleLib = fs.readFileSync(require.resolve("particle_library/main.js"), "utf8");
+
+>>>>>>> Stashed changes
 console.log(particleLib);
 serve(app, 3000);
