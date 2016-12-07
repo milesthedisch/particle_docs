2b1bf053b141b48b3e79da19f7e42264a8bec68c
Miles Disch <milesthedisch@gmail.com>
2016-12-07 21:26:31 +1100

    Add doc blocks.

--- public/js/fetchExample.js
+++ public/js/fetchExample.js
@@ -1,7 +1,8 @@
-/* eslint no-var: 0 */
+/* eslint no-var: 0, max-len: 0 */
 (function IIFE(window, document) {
   var DEFAULT_EXAMPLE = "@1";
 
+  /* shims */
   window.$ = function qs(selector, baseNode) {
     return document.querySelector(selector, baseNode);
   };
@@ -9,7 +10,13 @@
   window.$$ = function qsAll(selector, baseNode) {
     return document.querySelectorAll(selector, baseNode);
   };
+  /* shims */
 
+  /**
+   * [fetchExample description]
+   * @param  {[type]} id [description]
+   * @return {[type]}    [description]
+   */
   var fetchExample = function fetchExample(id) {
     return fetch("/examples/" + id)
     .then(function(response) {
@@ -22,18 +29,34 @@
     });
   };
 
+  /**
+   * [injectSrc description]
+   * @param  {[type]} src   [description]
+   * @param  {[type]} frame [description]
+   * @return {[type]}       [description]
+   */
   var injectSrc = function injectSrc(src, frame) {
     frame.srcdoc = src;
     return frame;
   };
 
-  var renderFrame = function renderFrame(iframe) {
+  /**
+   * [appendFrame description]
+   * @param  {[type]} iframe [description]
+   * @return {[type]}        [description]
+   */
+  var appendFrame = function appendFrame(iframe) {
     return document.querySelector(".frame_container").appendChild(iframe);
   };
 
+  /**
+   * [createFrame description]
+   * @param  {[type]} name [description]
+   * @return {[type]}      [description]
+   */
   var createFrame = function createFrame(name) {
     if (!name || typeof name !== "string") {
-      throw new Error("Not a valid name for a id.");
+      throw new Error(name + " Not a valid name for a id.");
     }
 
     const iframe = document.createElement("iframe");
@@ -47,31 +70,41 @@
     return iframe;
   };
 
+  /**
+   * [loadInIframe description]
+   * @param  {[type]} name [description]
+   * @return {[type]}      [description]
+   */
   var loadInIframe = function loadInIframe(name) {
     const frame = createFrame(name);
 
     return fetchExample(name)
     .then((src) => injectSrc(src, frame))
-    .then((newFrame) => renderFrame(newFrame));
+    .then((newFrame) => appendFrame(newFrame));
   };
 
-  var delegateListItems = function delegateListItems(elm, callback) {
-    elm.addEventListener("click", function(e) {
-      e.preventDefault();
-      console.log(e.target);
-      if (e.target.tagName = "A") {
-        return callback(null, e.target.text);
-      }
-
-      return callback(new Error("No target matched"));
-    });
+  /**
+   * isElement checks if a element is a DOM node.
+   * @param  {Object}  obj
+   * @return {Boolean}
+   */
+  var isElement = function isElm(obj) {
+    return obj instanceof HTMLElement;
   };
 
+  /**
+   * mapText takes an elment list and return a array of textNodes.
+   * @param  {DOMElememt} elm   DOMElememt
+   * @return {Array}            Array
+   */
   var mapText = function mapText(elm) {
     var elmList = $$(elm, document);
     var textNodes = [];
 
-    // We need to use a for `of` loop here cause its a NodeList and not an array.
+    /*
+      We need to use a for `of` loop here cause its a NodeList and not an
+      array.
+    */
     for (item of elmList) {
       textNodes.push(item.text);
     }
@@ -79,22 +112,49 @@
     return textNodes;
   };
 
+  /**
+   * setHash of URL
+   * @param {String} hash
+   */
   var setHash = function setHash(hash) {
     window.location.hash = hash || "";
   };
 
-  // Initalise the application.
+  /**
+   * elmDelegator delegate items
+   * @param  {DOMElement} elm         The parent element of the delegates.
+   * @param  {Function}   checkTarget Boolean to check which elements to delegate to.
+   * @param  {Function}   callback    A callback that is passed a error as its first 
+   *                                     argugmet and second argument as the delegate.
+   */
+  var elmDelegator = function elmDelegator(elm, checkTarget, callback) {
+    if (!isElement(elm)) throw new Error(elm + " needs to be a element.");
+    if (elm.length) throw new Error(elm + " needs to be element list");
+
+    elm.addEventListener("click", function(e) {
+      e.preventDefault();
+
+      if (checkTarget(e.target)) {
+        return callback(null, e.target);
+      }
+
+      return callback(new Error("No target matched"));
+    });
+  };
+
   document.addEventListener("DOMContentLoaded", function() {
     var hash = window.location.hash;
     var textNodes = mapText(".list_container li a");
 
-    delegateListItems($(".list_container"), function(err, target) {
+    elmDelegator($(".list_container"), function check(elm) {
+      return elm.tagName === "A";
+    }, function(err, target) {
       if (err) {
         throw err;
       }
 
       setHash(hash);
-      loadInIframe(target);
+      loadInIframe(target.text);
     });
 
     // If theres a page fragment load the right example.
