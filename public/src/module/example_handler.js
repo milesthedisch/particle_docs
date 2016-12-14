befb638a6dcc1193c56c5685afc96460ab6fa4a9
Miles Disch <milesthedisch@gmail.com>
2016-12-14 22:00:22 +1100

    Refactor fetch example

--- public/src/module/example_handler.js
+++ public/src/module/example_handler.js
@@ -1,39 +1,39 @@
 /**
-   * [fetchExample description]
-   * @param  {[type]} id [description]
-   * @return {[type]}    [description]
-   */
-  var fetchExample = function fetchExample(id) {
-    return fetch("/examples/" + id)
-    .then(function(response) {
-      return response.text().then(function(txt) {
-        return txt;
-      });
-    })
-    .catch(function(err) {
-      console.error(new Error(err));
+ * [fetchExample description]
+ * @param  {[type]} id [description]
+ * @return {[type]}    [description]
+ */
+const fetchExample = function fetchExample(id) {
+  return fetch("/examples/" + id)
+  .then(function(response) {
+    return response.text().then(function(txt) {
+      return txt;
     });
-  };
+  })
+  .catch(function(err) {
+    console.error(new Error(err));
+  });
+};
 
-  /**
-   * [exampleExists description]
-   * @param  {[type]} example [description]
-   * @return {[type]}         [description]
-   */
-  var exampleExists = function exampleExists(example) {
-    if (!example) return false;
+/**
+ * [exampleExists description]
+ * @param  {[type]} example [description]
+ * @return {[type]}         [description]
+ */
+const exampleExists = function exampleExists(example) {
+  if (!example) return false;
 
-    var id;
+  let id;
 
-    try {
-      id = getFrame(example)
-        .attributes["data-value"]
-        .nodeValue;  
-    } catch (e) {
-      if (e) {
-        id = false;  
-      }
+  try {
+    id = getFrame(example)
+      .attributes["data-value"]
+      .nodeValue;
+  } catch (e) {
+    if (e) {
+      id = false;
     }
-
+  } finally {
     return id === example;
-  };
+  }
+};
