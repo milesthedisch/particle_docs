af950bb02105636384c4ac1ee1ea6f226d23d1f7
Miles Disch <milesthedisch@gmail.com>
2017-01-01 13:27:06 +1100

    Finish circle example.

--- examples/a4/circle.js
+++ examples/a4/circle.js
@@ -1,45 +1,57 @@
-'use strict';
+"use strict";
 
-window.onload = function () {
-
+window.onload = function() {
   const rAF = window.requestAnimationFrame;
-  // When using id's the constiable is exposed 
+  // When using id's the constiable is exposed
   const canvas = a;
   const ctx = a.getContext("2d");
   const w = canvas.width = window.innerWidth;
   const h = canvas.height = window.innerHeight;
 
-  const particle = particleLib.Particle;
+  const particle = new particleLib.Particle();
+  const vec = new particleLib.Vector();
+  const shapes = new particleLib.Shapes(ctx, document);
 
   const cx = w * .5;
   const cy = h * .5;
-  const objRadius = 10;
+  const objRadius = 2;
   const radius = cy - objRadius;
-
-  const numObjects = 20;
+  const numObjects = 10;
   const slice = 2 * Math.PI / numObjects;
-  let x; 
+
+  let x;
   let y;
 
   const arr = [];
-  for (let i = 0; i < numObjects; i++) {
-    arr.push(new Particle());
+  for (let i = 0; i < numObjects; i += 1) {
+    arr.push(particle.create({
+      velocity: i,
+      position: vec.create(),
+    }));
   }
-      
-  arr
-  .map(function (x) { return x * slice})
-  .forEach(function(angle) {
-    x = cx + Math.cos(angle) * radius;
-    y = cy + Math.sin(angle) * radius;
-    ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2, false);
-    ctx.fill();
-  });
-  // If the window is resizes fill the page again.
+
+  (function animate() {
+    arr
+    .map(function(particle) {
+      particle.set("velocity", particle.get("velocity") * slice);
+      return particle;
+    })
+    .forEach(function(particle) {
+      x = cx + Math.cos(particle.get("velocity")) * radius;
+      y = cy + Math.sin(particle.get("velocity")) * radius;
+
+      particle.get("position").set("x", x);
+      particle.get("position").set("y", y);
+
+      shapes.circle(x, y, objRadius, 0, Math.PI * 2, false);
+    });
+    rAF(animate);
+  })();
+
+
+      // If the window is resizes fill the page again.
   window.onresize = function() {
     w = canvas.width = window.innerWidth;
     h = canvas.height = window.innerHeight;
-  }
-
-}
-
+  };
+};
