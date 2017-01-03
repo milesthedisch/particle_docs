4e3b21567478c3689b710e70813f00d53b6c6dd6
miles disch <milesthedisch@gmail.com>
2017-01-03 12:22:26 +1100

    Update particle_lib dep to use git link.

--- ex-config.js
+++ ex-config.js
@@ -1,66 +1,66 @@
 module.exports = { 	
 	"a1": { 
     "name": "a1", 
-    "js": "/Users/milesdisch/dev/Home/Side/particle_docs/examples/a1/main.js"
+    "js": "/Users/milesthedisch/dev/particle_docs/examples/a1/main.js"
   },
 	"a11": { 
     "name": "a11", 
-    "js": "/Users/milesdisch/dev/Home/Side/particle_docs/examples/a11/main.js"
+    "js": "/Users/milesthedisch/dev/particle_docs/examples/a11/main.js"
   },
 	"a12": { 
     "name": "a12", 
-    "js": "/Users/milesdisch/dev/Home/Side/particle_docs/examples/a12/main.js"
+    "js": "/Users/milesthedisch/dev/particle_docs/examples/a12/main.js"
   },
 	"a12-1": { 
     "name": "a12-1", 
-    "js": "/Users/milesdisch/dev/Home/Side/particle_docs/examples/a12-1/main.js"
+    "js": "/Users/milesthedisch/dev/particle_docs/examples/a12-1/main.js"
   },
 	"a12-2": { 
     "name": "a12-2", 
-    "js": "/Users/milesdisch/dev/Home/Side/particle_docs/examples/a12-2/main.js"
+    "js": "/Users/milesthedisch/dev/particle_docs/examples/a12-2/main.js"
   },
 	"a2": { 
     "name": "a2", 
-    "js": "/Users/milesdisch/dev/Home/Side/particle_docs/examples/a2/trig.js"
+    "js": "/Users/milesthedisch/dev/particle_docs/examples/a2/trig.js"
   },
 	"a3": { 
     "name": "a3", 
-    "js": "/Users/milesdisch/dev/Home/Side/particle_docs/examples/a3/main.js"
+    "js": "/Users/milesthedisch/dev/particle_docs/examples/a3/main.js"
   },
 	"a4": { 
     "name": "a4", 
-    "js": "/Users/milesdisch/dev/Home/Side/particle_docs/examples/a4/circle.js"
+    "js": "/Users/milesthedisch/dev/particle_docs/examples/a4/circle.js"
   },
 	"a4-5": { 
     "name": "a4-5", 
-    "js": "/Users/milesdisch/dev/Home/Side/particle_docs/examples/a4-5/lissajous_curves.js"
+    "js": "/Users/milesthedisch/dev/particle_docs/examples/a4-5/lissajous_curves.js"
   },
 	"a5": { 
     "name": "a5", 
-    "js": "/Users/milesdisch/dev/Home/Side/particle_docs/examples/a5/arctangent.js"
+    "js": "/Users/milesthedisch/dev/particle_docs/examples/a5/arctangent.js"
   },
 	"a5-5": { 
     "name": "a5-5", 
-    "js": "/Users/milesdisch/dev/Home/Side/particle_docs/examples/a5-5/5.5.js"
+    "js": "/Users/milesthedisch/dev/particle_docs/examples/a5-5/5.5.js"
   },
 	"a7": { 
     "name": "a7", 
-    "js": "/Users/milesdisch/dev/Home/Side/particle_docs/examples/a7/velocity.js"
+    "js": "/Users/milesthedisch/dev/particle_docs/examples/a7/velocity.js"
   },
 	"a8": { 
     "name": "a8", 
-    "js": "/Users/milesdisch/dev/Home/Side/particle_docs/examples/a8/accel.js"
+    "js": "/Users/milesthedisch/dev/particle_docs/examples/a8/accel.js"
   },
 	"a8-5": { 
     "name": "a8-5", 
-    "js": "/Users/milesdisch/dev/Home/Side/particle_docs/examples/a8-5/fireworks.js"
+    "js": "/Users/milesthedisch/dev/particle_docs/examples/a8-5/fireworks.js"
   },
 	"a9": { 
     "name": "a9", 
-    "js": "/Users/milesdisch/dev/Home/Side/particle_docs/examples/a9/main.js"
+    "js": "/Users/milesthedisch/dev/particle_docs/examples/a9/main.js"
   },
 	"normalizeEx": { 
     "name": "normalizeEx", 
-    "js": "/Users/milesdisch/dev/Home/Side/particle_docs/examples/normalizeEx/main.js"
+    "js": "/Users/milesthedisch/dev/particle_docs/examples/normalizeEx/main.js"
   },
  }
