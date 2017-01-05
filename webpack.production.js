5684834aa36dc22546d355f208916cf95f8d104a
Miles Disch <milesthedisch@gmail.com>
2017-01-05 11:33:08 +1100

    Add production config.

---
+++ webpack.production.js
@@ -0,0 +1,15 @@
+const common = require("./webpack.common.js");
+const webpack = require("webpack");
+const extend = require("extend");
+const path = require("path");
+
+module.exports = extend(true, {
+  entry: [
+    "whatwg-fetch",
+    path.resolve("./public/src/app.js"),
+  ],
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}}),
+  ],
+}, common);
+
