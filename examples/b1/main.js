f8f58753037b09dcaa8738c07840ebd5e0328f05
Miles Disch <milesthedisch@gmail.com>
2017-01-08 22:18:30 +1100

    Planets are now working.

--- examples/b1/main.js
+++ examples/b1/main.js
@@ -1,23 +1,40 @@
 
 window.onload = function () {
+  const particle = new particleLib.Particle();
+  const vector = new particleLib.Vector();
+  const util = particleLib.Utils;
 
-  // When using id's the variable is exposed 
-  var canvas = a;
-    rAF = window.requestAnimationFrame,
-    ctx = a.getContext("2d"),
-    w = canvas.width = window.innerWidth,
-    h = canvas.height = window.innerHeight,
-    sun = particle.create(w / 2, h / 2, 0, 0),
-    planet = particle.create(w / 2 + 200, h / 2, 10, -Math.PI / 2),
-    jupiter = particle.create(w / 2 + 500, h / 2, 9, -Math.PI / 2)
+  // When using id's the variable is exposed //
+  const canvas = a;
+  const rAF = window.requestAnimationFrame;
+  const ctx = a.getContext("2d");
 
-  sun.mass = 40000;
-  jupiter.mass = 1;
+  let w = canvas.width = window.innerWidth;
+  let h = canvas.height = window.innerHeight;
+
+  const sun = particle.create({
+    position: vector.create(w / 2, h / 2),
+  });
+
+  const planet = particle.create({
+    position: vector.create(w / 4, h / 4),
+    direction: -Math.PI / 2,
+    magnitude: 5,
+  });
+
+  const jupiter = particle.create({
+    position: vector.create(w / 4, h / 2),
+    magnitude: 7,
+    direction: -Math.PI / 2,
+  });
+
+  sun.set("mass", 4000);
+  planet.set("mass", 10);
+  jupiter.set("mass", 50);
 
   update();
- 
+
   function update() {
-    
     ctx.clearRect(0, 0, w, h);
 
     planet.gravitateTo(sun);
@@ -26,18 +43,18 @@ window.onload = function () {
     planet.update();
 
     ctx.beginPath();
-    ctx.fillStyle = '#ffff00';
-    ctx.arc(sun.position.getX(), sun.position.getY(), 20, 0, Math.PI * 2, false);
+    ctx.fillStyle = "#ffff00";
+    ctx.arc(sun.get("position").get("x"), sun.get("position").get("y"), 20, 0, Math.PI * 2, false);
     ctx.fill();
 
     ctx.beginPath();
-    ctx.fillStyle = '#00ffff';
-    ctx.arc(planet.position.getX(), planet.position.getY(), 5, 0, Math.PI * 2, false);
+    ctx.fillStyle = "#00ffff";
+    ctx.arc(planet.get("position").get("x"), planet.get("position").get("y"), 5, 0, Math.PI * 2, false);
     ctx.fill();
 
     ctx.beginPath();
-    ctx.fillStyle = '#0000ff';
-    ctx.arc(jupiter.position.getX(), jupiter.position.getY(), 10, 0, Math.PI * 2, false);
+    ctx.fillStyle = "#0000ff";
+    ctx.arc(jupiter.get("position").get("x"), jupiter.get("position").get("y"), 10, 0, Math.PI * 2, false);
     ctx.fill();
 
     rAF(update);
@@ -47,6 +64,5 @@ window.onload = function () {
   window.onresize = function() {
     w = canvas.width = window.innerWidth;
     h = canvas.height = window.innerHeight;
-  }
-
-}
+  };
+};
