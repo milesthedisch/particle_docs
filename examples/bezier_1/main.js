5b126cdada2232f88117fb417d52502b7b156c35
Miles Disch <milesthedisch@gmail.com>
2017-03-05 16:02:30 +1100

    Added beizer_1 example.

--- examples/bezier_1/main.js
+++ examples/bezier_1/main.js
@@ -3,29 +3,16 @@ window.onload = function() {
   const canvas = a;
   const ctx = a.getContext("2d");
 
-  const utils = particleLib.Utils;
+  const utils = new particleLib.Utils();
   const vector = new particleLib.Vector();
-  const shapes = new particleLib.Shapes(ctx, document);
+  const shapes = new particleLib.Shapes();
   const particle = new particleLib.Particle();
 
   let w = canvas.width = window.innerWidth;
   let h = canvas.height = window.innerHeight;
 
-  let x;
-  let y;
-
-  document.addEventListener("mousemove", function(e) {
-    x = utils.roundToMultiple(e.clientX, 40);
-    y = utils.roundToMultiple(e.clientY, 40);
-    console.log(x, y);
-  });
-
   (function render() {
     ctx.clearRect(0, 0, w, h);
-
-    shapes.grid(w, h, 40);
-    shapes.circle(x, y, 20);
-
     rAF(render);
   })();
 
