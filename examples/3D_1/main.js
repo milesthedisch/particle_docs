2cae6135bbd12bd78eb6ed6518fff1d280330536
Miles Disch <milesthedisch@gmail.com>
2017-03-19 12:32:06 +1100

    Update 3D example code.

--- examples/3D_1/main.js
+++ examples/3D_1/main.js
@@ -4,6 +4,12 @@ window.onload = function() {
   const canvas = a;
   const ctx = a.getContext("2d");
 
+  // Libs //
+  const utils = particleLib.Utils;
+  const vector = new particleLib.Vector();
+  const shapes = new particleLib.Shapes(ctx, document);
+  const particle = new particleLib.Particle();
+
   // Boundries //
   let w = canvas.width = window.innerWidth;
   let h = canvas.height = window.innerHeight;
@@ -11,39 +17,48 @@ window.onload = function() {
   // Focal length //
   const fl = 300;
 
-  // Position of Shape in 3D space //
-  const shapePos = {
-    x: 500,
-    y: 300,
-    z: 500,
-  };
+  let squares = [];
+  const numsquares = 100;
 
-
-  // Libs //
-  const utils = particleLib.Utils;
-  const vector = new particleLib.Vector();
-  const shapes = new particleLib.Shapes(ctx, document);
-  const particle = new particleLib.Particle();
+  for(let i = 0; i < numsquares; i++) {
+    squares[i] = {
+      x: utils.randomBetween(-1000, 1000),
+      y: utils.randomBetween(-1000, 1000),
+      z: utils.randomBetween(0, 10000),
+    };
+  }
 
   // Move the context to the center of the screen.
   ctx.translate(w/2, h/2);
+  let i = 10;
 
-  // Clear the whole canvas //
-  ctx.clearRect(-w/2, -h/2, w/2, h/2);
-  ctx.save();
+  update();
 
-  (function render() {
+  function update() {
+    i += 0.1;
+    // Clear the whole canvas //
+    ctx.clearRect(-w/2, -h/2, w, h);
+    console.log(i);
+    squares.forEach(function(shape) {
 
-    // Perspective fomulae that gives us the coordinate based on the perspective //
-    // focalLength / (focalLength + distance) //
-    var perspective = fl / (fl + shapePos.z);
-    
-    ctx.translate(shapePos.x * perspective, shapePos.y * perspective);
-    ctx.scale(perspective, perspective);
-    ctx.fillRect(-100, -100, 200, 200);
+      ctx.save();
+      // Perspective fomulae that gives us the coordinate based on the perspective //
+      // focalLength / (focalLength + distance) //
+      var perspective = fl / (fl + shape.z);  
 
-    rAF(render);
-  })();
+      ctx.translate(shape.x * perspective, shape.y * perspective);
+      ctx.scale(perspective, perspective);
+      ctx.fillRect(-100, -100, 200, 200);
+      ctx.restore();
+
+      shape.z -= i;
+      if(shape.z < 0) {
+        shape.z = 0;
+      }
+    });
+
+    rAF(update);
+  }
 
   // If the window is resizes fill the page again.
   window.onresize = function() {
