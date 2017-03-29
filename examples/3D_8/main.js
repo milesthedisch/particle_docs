a60ab2ed33f7f2f5b0510a1b691a88e05660f65c
Miles Disch <milesthedisch@gmail.com>
2017-03-30 10:12:01 +1100

    Creating 3D cube model.

--- examples/3D_8/main.js
+++ examples/3D_8/main.js
@@ -2,7 +2,19 @@ window.onload = function() {
   const rAF = window.requestAnimationFrame;
   const canvas = a;
   const ctx = a.getContext("2d");
-  
+  const fl = 300;
+  const points = [];
+
+  // Model of a cube.
+  points[0] = {x: -500, y: -500, z: 1000};
+  points[1] = {x: 500, y: -500, z: 1000};
+  points[2] = {x: 500, y: -500, z: 500};
+  points[3] = {x: -500, y: -500, z: 500};
+  points[4] = {x: -500, y: 500, z: 1000};
+  points[5] = {x: 500, y: 500, z: 1000};
+  points[6] = {x: 500, y: 500, z: 500};
+  points[7] = {x: -500, y: 500, z: 500};
+
   // Libs
   const utils = particleLib.Utils;
   const vector = new particleLib.Vector();
@@ -13,10 +25,24 @@ window.onload = function() {
   let w = canvas.width = window.innerWidth;
   let h = canvas.height = window.innerHeight;
 
+  context.tranlsate(w/2, h/2);
+
   (function render() {
-    ctx.clearRect(0, 0, w, h);
+    ctx.clearRect(-w/2, -h/2, w, h);
+    project(points);
     rAF(render);
   })();
+
+  function project(points) {
+    for (let i = 0; points.lenght; i++) {
+      let p = points[i];
+          scale = fl / (fl + p.z);
+
+      p.sx = scale * p.x;
+      p.sy = scale * p.y; 
+    }
+  }
+
 
   // If the window is resizes fill the page again.
   window.onresize = function() {
