41b55c5628bded94438bd8c9d48cd67534f7a54e
Miles Disch <milesthedisch@gmail.com>
2017-04-01 15:02:27 +1100

    Add 3D cube example.

--- examples/3D_8/main.js
+++ examples/3D_8/main.js
@@ -1,9 +1,11 @@
 window.onload = function() {
+  window.focus(); 
   const rAF = window.requestAnimationFrame;
   const canvas = a;
   const ctx = a.getContext("2d");
   const fl = 300;
   const points = [];
+  var needsUpdate = true;
 
   // Model of a cube.
   points[0] = {x: -500, y: -500, z: 1000};
@@ -25,19 +27,49 @@ window.onload = function() {
   let w = canvas.width = window.innerWidth;
   let h = canvas.height = window.innerHeight;
 
+  const translateBox = translatePoints(points);
+
+  document.body.addEventListener("keydown", function(event) {
+    switch(event.keyCode) {
+    case 37: // left
+      translateBox(-20, 0, 0);
+      break;
+    case 38: // up
+      if (event.shiftKey) {
+        translateBox(0, 0, 20);
+      } else {
+        translateBox(0, -20, 0);
+      }
+      break;
+    case 39: // right
+      translateBox(20, 0, 0);
+      break;
+    case 40: // down
+      if (event.shiftKey) {
+        translateBox(0, 0, -20);
+      } else {
+        translateBox(0, 20, 0);
+      }
+      break;  
+    }
+  });
+
   ctx.translate(w/2, h/2);
 
   (function render() {
-    ctx.clearRect(-w/2, -h/2, w, h);
-    project(points);
-    drawModel(points);
+    if (needsUpdate) {
+      ctx.clearRect(-w/2, -h/2, w, h);
+      project(points);
+      drawModelSquare(points);
+    }
     rAF(render);
   })();
 
-  function drawModel(points) {
+  function drawModelSquare(points) {
     let p = points.map(function(point) { 
       return {x: point.sx, y: point.sy};
     });
+
     shapes.drawLinePoints(p[0], p[1], p[2], p[3], p[0]);
     shapes.drawLinePoints(p[0], p[4]);
     shapes.drawLinePoints(p[1], p[5]);
@@ -45,6 +77,17 @@ window.onload = function() {
     shapes.drawLinePoints(p[3], p[7]);
     shapes.drawLinePoints(p[4], p[5], p[6], p[7], p[4]);
   }
+
+  function translatePoints(ps) {
+    return function translateModel(x, y, z) {
+      for (let i = 0; i < ps.length; i++) {
+        points[i].x += x;
+        points[i].y += y;
+        points[i].z += z;
+      }
+      needsUpdate = true;
+    };
+  };
 
   function project(points) {
     for (let i = 0; i < points.length; i++) {
@@ -55,7 +98,6 @@ window.onload = function() {
       p.sy = scale * p.y; 
     }
   }
-
 
   // If the window is resizes fill the page again.
   window.onresize = function() {
