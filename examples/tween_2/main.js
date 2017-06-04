b5e0bb9d4b7b33130acd4f570b5e836fa2d03f5f
Miles Disch <milesthedisch@gmail.com>
2017-06-04 12:00:51 +1000

    Add second tween example with delays and variable fps.

--- examples/tween_2/main.js
+++ examples/tween_2/main.js
@@ -1,22 +1,45 @@
 window.onload = function() {
-  const rAF = window.requestAnimationFrame;
   const canvas = a;
   const ctx = a.getContext("2d");
-  
+
   // Libs
   const utils = particleLib.Utils;
   const vector = new particleLib.Vector();
   const shapes = new particleLib.Shapes(ctx, document);
   const particle = new particleLib.Particle();
-  
-  // Boundries
+
   let w = canvas.width = window.innerWidth;
   let h = canvas.height = window.innerHeight;
 
-  (function render() {
+  const YAT = particleLib.YAT.init({
+    clock: particleLib.Clock,
+    fps: 10,
+  });
+
+  const t1 = YAT.create({
+    obj: {
+      x: 10,
+      y: 10,
+    },
+    props: {
+      x: 100,
+      y: 100,
+    },
+    duration: 10000,
+  });
+
+  YAT._clock.on("render", function render() {
+    ctx.font = "20px serif";
     ctx.clearRect(0, 0, w, h);
-    rAF(render);
-  })();
+    ctx.fillText(`${JSON.stringify(YAT.tweens[0].state)}`, 30, 30);
+    shapes.circle(t1.state.x, t1.state.y, 20);
+  });
+
+
+  YAT.startAll();
+  setTimeout(function() {
+    t1.delay(1000);
+  }, 2000);
 
   // If the window is resizes fill the page again.
   window.onresize = function() {
