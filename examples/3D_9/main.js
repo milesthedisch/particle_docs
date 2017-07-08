037c8c4be46f8e1b241f7e8f5a42071a66fd9ade
Miles Disch <milesthedisch@gmail.com>
2017-07-08 18:35:45 +1000

    Update example 9 star field.

--- examples/3D_9/main.js
+++ examples/3D_9/main.js
@@ -17,11 +17,11 @@ window.onload = function() {
   const fl = 300;
   const particleNum = 1000;
   let baseAngle = 3.14;
+  let xAngleSpeed = 0;
 
   // Boundries
   let w = canvas.width = window.innerWidth;
   let h = canvas.height = window.innerHeight;
-
 
   const spheres = [...new Array(particleNum)].map(sphere);
 
@@ -35,13 +35,13 @@ window.onload = function() {
   function sphere(val, idx) {
     let x = _.randomBetween(-w/2, w/2);
     let y = _.randomBetween(-h/2 - 500, h/2 + 500);
-    let sz = _.randomBetween(2, 10);
+    let sz = _.randomBetween(10, 20);
 
     return {
       sz,
       y,
-      x: Math.cos(baseAngle) * x,
-      z: Math.sin(baseAngle + idx) * 300,
+      x: Math.cos(idx) * y,
+      z: Math.sin(idx) * 300 + 2000,
     };
   }
 
@@ -49,21 +49,27 @@ window.onload = function() {
     const x = utils.map(mvCoord.x, 0, w, -100, 100);
     const y = utils.map(mvCoord.y, 0, h, -100, 100);
     baseAngle += 0.01;
+    xAngleSpeed += 0.01 * (y / 30);
     spheres.forEach((s, idx) => {
-      if (s.y > h + 1200 || s.y < -h/2) {
-        s.y = (h * Math.random()) + 1000;
+      s.sz -= 0.1 * Math.random();
+
+      if (s.y > h + 4000 || s.y < -h/2 || s.sz <= 0) {
+        s.y = (h * Math.random()) + 4000;
+        s.sz = _.randomBetween(10, 20);
       }
-      s.y += y * -0.1;
-      s.x += Math.cos(baseAngle) * 3;
-      s.z += Math.sin(baseAngle) * 0.5;
+
+      s.y += -10;
+      s.x += Math.cos(idx) * 20;
+      s.z += Math.sin(idx) * 20;
     });
   }
 
   function draw() {
+    ctx.clearRect(-w/2, -h/2, w, h);
+    ctx.fillRect(-w/2, -h/2, w, h, "black");
     spheres.forEach((s) => {
       ctx.save();
       // Save
-
 
       prsp = _.perspective(fl, s.z);
       ctx.globalAlpha = _.map(s.y * prsp, -h, prsp * h, 0, 1);
@@ -71,18 +77,16 @@ window.onload = function() {
       ctx.scale(prsp, prsp);
       ctx.translate(s.x, s.y);
 
-      shapes.circle(0, 0, s.sz);
+      shapes.circle(0, 0, s.sz, "#FFFFFF");
 
       // Restore
       ctx.restore();
     });
   }
 
-
   ctx.translate(w/2, h/2);
 
   function render() {
-    ctx.clearRect(-w/2, -h/2, w, h);
     update();
     draw();
     rAF(render);
